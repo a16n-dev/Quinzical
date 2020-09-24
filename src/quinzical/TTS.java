93cb16cad122ff4f3f3fb2a340f8057605b2193a
Peter Goedeke <peterbgoedeke@gmail.com>
2020-09-24 17:19:50 +1200

    Added queuing & execution of speaking and clearing of queue

--- src/quinzical/TTS.java
+++ src/quinzical/TTS.java
@@ -24,6 +24,45 @@ public class TTS {
         return tts;
     }
 
+    private void speakNext() {
+        Task<Void> task = new Task<Void>() {
+			@Override
+			protected Void call() throws Exception {
+                if(processQueue.peek() != null) {
+                    var builder = processQueue.poll();
+        
+                    try {
+                        Process p = builder.start();
+                        try {
+                            p.waitFor();
+                            speakNext();
+                        }
+                        catch (InterruptedException e) {
+                            System.out.println(e);
+                        }
+                    }
+                    catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+				return null;
+			}
+        };
+        new Thread(task).start();
+    }
+
+    public void speak(String text) {
+        boolean alreadyRunning = processQueue.peek() != null;
+
+        ProcessBuilder builder = new ProcessBuilder("espeak", "-x", text, "-a", Integer.toString(volume));
+        processQueue.add(builder);
+        if (!alreadyRunning) speakNext();
+    }
+
+    public void clearQueue() {
+        processQueue.clear();
+    }
+
     public void setVolume(int volume) {
         this.volume = volume;
     }
