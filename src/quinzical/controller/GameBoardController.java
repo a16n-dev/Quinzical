a7f62559a0116bef93cd86013ae099a370af3bc7
Alexander Nicholson <alexn400@gmail.com>
2020-10-04 13:47:56 +1300

    Improve game board

--- src/quinzical/controller/GameBoardController.java
+++ src/quinzical/controller/GameBoardController.java
@@ -41,18 +41,24 @@ public class GameBoardController {
 			// Place label
 			Label label = new Label(category);
 			label.setTextFill(Color.web("#fff"));
+			label.setStyle("-fx-font: 16 arial;");
 			grid.add(label, i, 0);
 
 			// Place buttons
 			ArrayList<Question> questions = game.getQuestionsByCategory(category);
-
+			int pos = 0;
 			for (int j = 0; j < questions.size(); j++) {
 				final Integer intJ = j;
 				Question question = questions.get(intJ);
 				// Place label
 				Button button = new Button("$" + question.getValue());
 
-				if (question.isAnswered() || !available) {
+				button.setStyle("-fx-font: 16 arial;");
+				button.setStyle("-fx-padding: 8px 16px 8px 16px;");
+
+				if(!question.isAnswered()){
+					pos+=1;
+				if (!available) {
 					// Disable button if the question has been attempted
 					button.setDisable(true);
 				} else {
@@ -64,7 +70,8 @@ public class GameBoardController {
 					});
 				}
 
-				grid.add(button, i, j + 1);
+				grid.add(button, i, pos);
+			}
 			}
 		}
 	}
