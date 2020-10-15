9d7a03ec7e4125c4d1d44af5764af6df8e39b1c3
Peter Goedeke <peterbgoedeke@gmail.com>
2020-10-15 22:41:01 +1300

    Refactor of Macron

--- src/main/java/quinzical/util/Macron.java
+++ src/main/java/quinzical/util/Macron.java
@@ -1,12 +1,13 @@
 package quinzical.util;
 
-import java.text.Normalizer;
 import java.util.HashMap;
 
+import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
@@ -15,8 +16,6 @@ public class Macron {
     private static Macron instance;
     private HashMap<Character, Character> pairs;
     private TextField fxInput;
-    private String oldText;
-    private int index;
 
     private Macron() {
         pairs = new HashMap<Character, Character>();
@@ -30,9 +29,6 @@ public class Macron {
         pairs.put('I', 'Ī');
         pairs.put('O', 'Ō');
         pairs.put('U', 'Ū');
-
-        oldText = "";
-        index = -1;
     }
 
     public static Macron getInstance() {
@@ -42,45 +38,38 @@ public class Macron {
         return instance;
     }
 
-    private String replaceWithMacron(String text, int index) {
-        var builder = new StringBuilder(text);
-        builder.setCharAt(index, pairs.get(text.charAt(index)));
-        return builder.toString();
-    }
-
     public void bind(TextField fxInput, Label fxMacronLetter, VBox fxMacronContainer) {
         this.fxInput = fxInput;
 
-        fxInput.setOnKeyTyped(e -> {
-            Character macron = pairs.get(e.getCharacter().charAt(0));
-            String newText = fxInput.getText();
-            if (macron != null) {
-                index = firstDifferenceIndex(oldText, newText);
-                double displayLocation = computeTextWidth(newText.substring(0, index));
-                positionGUI(fxMacronLetter, displayLocation);
+        fxInput.addEventFilter(KeyEvent.ANY, new EventHandler<KeyEvent>() {
+            @Override public void handle(KeyEvent keyEvent) {
+                if (keyEvent.getCode() == KeyCode.UP) {
+                    keyEvent.consume();
 
+                    int index = fxInput.getCaretPosition();
+                    if (index != 0) {
+                        Character atCursor = fxInput.getText().charAt(index - 1);
+                    
+                        if (pairs.get(atCursor) != null) {
+                            fxInput.replaceText(index - 1, index, Character.toString(pairs.get(atCursor)));
+                            fxMacronLetter.setVisible(false);
+                        }
+                    }
+                }
+            }
+        });
+
+        fxInput.setOnKeyReleased(e -> {
+            int index = fxInput.getCaretPosition();
+            Character macron = pairs.get(fxInput.getText().charAt(index - 1));
+            if (index != 00 && macron != null) {
+                positionGUI(fxMacronLetter, computeTextWidth(fxInput.getText().substring(0, index)));
                 fxMacronLetter.setText(Character.toString(macron));
                 fxMacronLetter.setVisible(true);
             }
             else {
                 fxMacronLetter.setVisible(false);
-                index = -1;
             }
-            oldText = newText;
-        });
-
-        fxInput.setOnKeyPressed(e -> {
-            e.consume();
-            if (e.getCode() == KeyCode.UP) {
-                if (index != -1) {
-                    String newText = replaceWithMacron(fxInput.getText(), index);
-                    fxInput.setText(newText);
-                    fxInput.positionCaret(index + 1);
-                    index = -1;
-                    fxMacronContainer.setVisible(false);
-                    oldText = newText;
-                }
-            }   
         });
     }
 
@@ -102,29 +91,6 @@ public class Macron {
     }
 
     private void positionGUI(Label fxLabel, double location) {
-        VBox.setMargin(fxLabel, new Insets(0, 0, 0, location + 13));
-    }
-
-    private int firstDifferenceIndex(String a, String b) {
-        int i = -1;
-        int smallestLength = Math.min(a.length(), b.length());
-        for (int j = 0; j < smallestLength; j++) {
-            if (Character.compare(a.charAt(j), b.charAt(j)) != 0) {
-                i = j;
-                break;
-            }
-        }
-        return i == -1 ? smallestLength : i;
-    }
-
-    /**
-     * Remove all macrons from a sentence
-     * 
-     * @param str The string to remove macrons from
-     * @return Macron-less string
-     */
-    private String sanitise(String str) {
-        str = Normalizer.normalize(str, Normalizer.Form.NFKD);
-        return str.toLowerCase().replaceAll("\\p{M}", "");
+        VBox.setMargin(fxLabel, new Insets(0, 0, 0, location));
     }
 }
