4acb534d8a788e4e5dd12d185a9bdc2f27022a36
Alexander Nicholson <alexn400@gmail.com>
2020-10-24 11:15:47 +1300

    Move member to seperate class

--- src/main/java/quinzical/model/MultiplayerGame.java
+++ src/main/java/quinzical/model/MultiplayerGame.java
@@ -61,31 +61,3 @@ public class MultiplayerGame extends QuinzicalGame {
     }
 }
 
-class Member {
-    private Avatar avatar;
-    private int score;
-    private boolean isHost;
-
-    public Member(Avatar avatar, int score) {
-        this.avatar = avatar;
-        this.score = score;
-        this.isHost = false;
-    }
-    public Member(Avatar avatar, int score, boolean isHost) {
-        this(avatar, score);
-        this.isHost = isHost;
-    }
-
-    public Avatar getAvatar() {
-        return avatar;
-    }
-    public int getScore() {
-        return score;
-    }
-    public void setScore(int score) {
-        this.score = score;
-    }
-    public boolean isHost() {
-        return isHost;
-    }
-}
